6adc3586cb2c24e4575072ee30f1cee8dcba9a2c
N1kola0s <97747012+N1kola0s@users.noreply.github.com>
2022-04-07 17:36:06 +0200

    add correction

--- assets/js/main.js
+++ assets/js/main.js
@@ -68,7 +68,13 @@ stampare le stesse informazioni su DOM sottoforma di stringhe */
 
     personasElement.append(card);
 
-    card.innerHTML = `   `;
+    card.innerHTML = ` 
+        <div class="card" style="width: 18rem;">
+            <img src="./" class="card-img-top" alt="...">
+            <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+        </div>  `;
     
 
   }
